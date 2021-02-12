7a35884701a96a87e7f447f545c3f1695781b924
mooniiDev <moonii@riseup.net>
2021-02-12 18:41:22 +0200

    Add functions to add and show books in library, update tooltip text and input style

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const myLibrary = [];
+
+function Books(title, author, pages, status) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+}
+
+function addBookToLibrary(title, author, pages, status) {
+  const book = new Books(title, author, pages, status);
+  myLibrary.push(book);
+  return myLibrary;
+}
+addBookToLibrary('Sapiens: A Brief History of Humankind', 'Yuval Noah Harari', '464', 'read');
+addBookToLibrary('The Lady of the Lake', 'Andrzej Sapkowski', '544', 'unread');
+
+function showBooksInLibrary() {
+  for (let i = 0; i < myLibrary.length; i += 1) {
+    const bookList = document.querySelector('#table-body');
+    const bookRow = document.createElement('tr');
+    bookRow.classList.add('book-info');
+    bookList.appendChild(bookRow);
+
+    const bookTitle = document.createElement('td');
+    bookTitle.textContent = myLibrary[i].title;
+    bookRow.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement('td');
+    bookAuthor.textContent = myLibrary[i].author;
+    bookRow.appendChild(bookAuthor);
+
+    const bookPages = document.createElement('td');
+    bookPages.textContent = myLibrary[i].pages;
+    bookRow.appendChild(bookPages);
+
+    const bookStatus = document.createElement('td');
+    const statusButton = document.createElement('button');
+    const statusSymbol = document.createElement('i');
+    if (myLibrary[i].status === 'read') {
+      statusSymbol.classList.add('fas', 'fa-check');
+    } else if (myLibrary[i].status === 'unread') {
+      statusSymbol.classList.add('fas', 'fa-times');
+    }
+    statusButton.appendChild(statusSymbol);
+    bookStatus.appendChild(statusButton);
+    bookRow.appendChild(bookStatus);
+
+    const bookDelete = document.createElement('td');
+    const deleteButton = document.createElement('button');
+    const deleteSymbol = document.createElement('i');
+    deleteSymbol.classList.add('fas', 'fa-trash-alt');
+    deleteButton.appendChild(deleteSymbol);
+    bookDelete.appendChild(deleteButton);
+    bookRow.appendChild(bookDelete);
+  }
+}
+showBooksInLibrary();
